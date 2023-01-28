bbccda9c17ed6182c2129a4c95e49b0b2daca971
DrFaustest <95448970+DrFaustest@users.noreply.github.com>
2023-01-28 13:22:39 -0600

    finished week 7 class

---
+++ Sfaust-Assignment7/Employee.java
@@ -0,0 +1,164 @@
+import java.text.NumberFormat;
+
+public class Employee {
+/*
+-firstName : String 
+-lastName : String 
+-employeeNum : int 
+-department : String 
+-jobTitle : String 
+-hoursWorked : double 
+-payRate : double 
+-currency : NumberFormat 
+*/
+/*
+<<constructor>>Employee(String fn, String ln, int en, String dept, String job, double pr) 
+<<constructor>>Employee(String fn, String ln, int en) 
+<<constructor>>Employee(Employee e) 
+<<constructor>>Employee() 
++getFirstName() : String 
++setFirstName(String fn) : void 
++getLastName() : String 
++setLastName(String ln) : void 
++getEmployeeNumber() : int 
++setEmployeeNumber(int en) : void 
++getDepartment() : String 
++setDepartment(String dept) : void 
++getJobTitle() : String 
++setJobTitle(String job) : void 
++getHoursWorked() : double 
++addHours() : void 
++addHours(double h) : void 
++resetHours() : void 
++getPayRate() : double 
++setPayRate(double pr) : void 
++calculateWeeklyPay() : double 
++printEmployee() : void
+*/
+private String firstName;
+private String lastName;
+private int employeeNum;
+private String department;
+private String jobTitle;
+private double hoursWorked;
+private double payRate;
+private NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+// main constructor that sets up full account
+public Employee(String fn, String ln, int en, String dept, String job, double pr)
+{
+    firstName = fn;
+    lastName = ln;
+    employeeNum = en;
+    department = dept;
+    jobTitle = job;
+    payRate = pr;
+    hoursWorked = 0;
+}
+//constructor with no balance
+public Employee(String fn, String ln, int en)
+{
+    this(fn, ln, en, "", "", 0);
+}
+// copy constructor
+public Employee(Employee e)
+{
+    firstName = e.getFirstName();
+    lastName = e.getLastName();
+    employeeNum = e.getEmployeeNumber();
+    department = e.getDepartment();
+    jobTitle = e.getJobTitle();
+    payRate = e.getPayRate();
+    hoursWorked = 0;
+}
+
+// default constructor
+public Employee()
+{
+    this("", "", 0, "", "", 0);
+}
+
+public String getFirstName() {
+    return firstName;
+}
+
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
+
+public int getEmployeeNumber() {
+    return employeeNum;
+}
+
+public void setEmployeeNumber(int employeeNum) {
+    this.employeeNum = employeeNum;
+}
+
+public String getDepartment() {
+    return department;
+}
+
+public void setDepartment(String department) {
+    this.department = department;
+}
+
+public String getJobTitle() {
+    return jobTitle;
+}
+
+public void setJobTitle(String jobTitle) {
+    this.jobTitle = jobTitle;
+}
+
+public double getHoursWorked() {
+    return hoursWorked;
+}
+
+public void addHours() {
+    this.hoursWorked++;
+}
+
+public void addHours(double hoursWorked) {
+    //check for negative hours
+    if (hoursWorked < 0) {
+        // the example doesnt't have anthying printed to the terminal indicating an error
+    } else {
+        this.hoursWorked += hoursWorked;
+    }
+}
+
+public void resetHours() {
+    this.hoursWorked = 0;
+}
+
+public double getPayRate() {
+    return payRate;
+}
+
+public void setPayRate(double payRate) {
+    this.payRate = payRate;
+}
+
+public double calculateWeeklyPay() {
+    return hoursWorked * payRate;
+}
+
+public void printEmployee() {
+    System.out.println("Name: " + firstName + " " + lastName);
+    System.out.println("ID: " + employeeNum);
+    System.out.println("Department: " + department);
+    System.out.println("Title: " + jobTitle);
+    System.out.println("Pay: " + currency.format(payRate));
+    System.out.println("Hours Worked: " + hoursWorked);
+}
+
+}
+
